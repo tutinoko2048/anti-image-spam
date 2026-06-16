60004e4b834be1a918bf0701094b0bcf1eab3271
RetoRuto9900K <tutinoko2048@gmail.com>
2026-06-16 14:56:12 +0900

    fix pm2 conf

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -2,8 +2,9 @@ module.exports = {
   apps: [
     {
       name: process.env.PM2_APP_NAME || "anti-image-spam",
-      script: "src/main.ts",
       interpreter: "bun",
+      script: "run",
+      args: "src/main.ts",
       env: {
         PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
       }
